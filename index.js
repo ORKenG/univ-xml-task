ce525e833abc5341ddd632aff7a96798092bc2cf
oburia4 <oleh.buria@capgemini.com>
2019-12-03 14:59:07 +0200

    initial master commit with node js start example

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+let app = express();
+
+app.use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs');
+
+
+  app.get('/', (req, res) => res.render('pages/index'));
+  app.get('/db', (req, res) => res.render('pages/db'));
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
