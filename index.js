ca9adab0d10d4d3e89d848e6ff0c0b6e70555cd0
denisLavreniuk <lavden99@gmail.com>
2019-12-03 16:45:21 +0200

    register

--- index.js
+++ index.js
@@ -10,5 +10,6 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 
   app.get('/', (req, res) => res.render('pages/index'));
+  app.get('/register', (req, res) => res.render('pages/register'));
   app.get('/db', (req, res) => res.render('pages/db'));
   app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
